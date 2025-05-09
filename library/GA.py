ee6258d4f7f27430d2774d5f87c20be667529e12
Inês Major <98774210+inesnmajor@users.noreply.github.com>
2025-05-09 11:59:53 +0100

    Selection algo

---
+++ library/GA.py
@@ -0,0 +1,105 @@
+from library.classes import FootballSolution, Team
+
+import random
+import numpy as np
+from copy import deepcopy
+import csv
+import pandas as pd
+
+def run_ga_test_single_child(selection_func, crossover_func, mutation_func,
+                             generations=100, pop_size=40, elitism=True, n_runs=30,
+                             crossover_prob=0.9, mutation_prob=0.3):
+    """
+    Runs a GA test assuming crossover produces a single child.
+    """
+    from copy import deepcopy
+    import random
+
+    all_runs_best_per_gen = []
+
+    for run in range(n_runs):
+        population = [FootballSolution() for _ in range(pop_size)]
+        best_per_gen = []
+
+        for gen in range(generations):
+            new_population = []
+
+            if elitism:
+                elite = deepcopy(max(population, key=lambda ind: ind.fitness()))
+                new_population.append(elite)
+
+            while len(new_population) < pop_size:
+                p1 = selection_func(population)
+                p2 = selection_func(population)
+
+                # Crossover (single child)
+                if random.random() < crossover_prob:
+                    child = crossover_func(p1, p2)
+                else:
+                    child = deepcopy(p1)
+
+                # Mutation
+                if random.random() < mutation_prob:
+                    child = mutation_func(None, child)
+
+                new_population.append(child)
+
+            population = new_population
+            best_gen_fitness = max(ind.fitness() for ind in population)
+            best_per_gen.append(best_gen_fitness)
+
+        all_runs_best_per_gen.append(best_per_gen)
+
+    return all_runs_best_per_gen
+
+
+def run_ga_test_two_children(selection_func, crossover_func, mutation_func,
+                             generations=100, pop_size=40, elitism=True, n_runs=30,
+                             crossover_prob=0.9, mutation_prob=0.3):
+    """
+    Runs a GA test assuming crossover produces two children.
+    """
+    from copy import deepcopy
+    import random
+
+    all_runs_best_per_gen = []
+
+    for run in range(n_runs):
+        population = [FootballSolution() for _ in range(pop_size)]
+        best_per_gen = []
+
+        for gen in range(generations):
+            new_population = []
+
+            if elitism:
+                elite = deepcopy(max(population, key=lambda ind: ind.fitness()))
+                new_population.append(elite)
+
+            while len(new_population) < pop_size:
+                p1 = selection_func(population)
+                p2 = selection_func(population)
+
+                # Crossover (two children)
+                if random.random() < crossover_prob:
+                    child1, child2 = crossover_func(p1, p2)
+                else:
+                    child1, child2 = deepcopy(p1), deepcopy(p2)
+
+                # Mutation on both
+                if random.random() < mutation_prob:
+                    child1 = mutation_func(None, child1)
+                if random.random() < mutation_prob:
+                    child2 = mutation_func(None, child2)
+
+                for child in [child1, child2]:
+                    if len(new_population) < pop_size:
+                        new_population.append(child)
+
+            population = new_population
+            best_gen_fitness = max(ind.fitness() for ind in population)
+            best_per_gen.append(best_gen_fitness)
+
+        all_runs_best_per_gen.append(best_per_gen)
+
+    return all_runs_best_per_gen
+
--- selection_analysis/best_selection.py
+++ selection_analysis/best_selection.py
@@ -1,24 +1,34 @@
 from library.selection import tournament_selection, fitness_proportionate_selection
+
 from library.crossover import crossover_blockwise_teams, crossover_position_based
+from library.crossover2child import crossover_blockwise_teams_two_offspring, crossover_position_based_two_offspring
+
 from library.mutation import mutate_global_position_permutation, mutate_random_position_swap, mutate_swap_between_teams
 from library.classes import FootballSolution, Team
+from library.GA import run_ga_test_single_child, run_ga_test_two_children
 
 import random
 import numpy as np
 from copy import deepcopy
 import csv
 import pandas as pd
+from itertools import product
 
 
-# possible combinations
+# Definir métodos disponíveis
 selection_methods = {
     "tournament": tournament_selection,
     "fitness_proportionate": fitness_proportionate_selection
 }
 
-crossover_methods = {
+crossover_methods_single_child = {
     "blockwise": crossover_blockwise_teams,
     "positionbased": crossover_position_based
+}
+
+crossover_methods_two_children = {
+    "blockwise_2child": crossover_blockwise_teams_two_offspring,
+    "positionbased_2child": crossover_position_based_two_offspring
 }
 
 mutation_methods = {
@@ -27,90 +37,53 @@ mutation_methods = {
     "between_teams": mutate_swap_between_teams
 }
 
-'''
-function to run tests for choosing the best selection algorithm
-we will run 30 times and take the median to evaluate the best selection algorithm 
-we will use 30 generations and population size of 20 to run this tests-- FOR NOW--
-'''
+all_xo_mut_combinations = list(product(
+    list(crossover_methods_single_child.items()) + list(crossover_methods_two_children.items()),
+    mutation_methods.items()
+))
+xo_mut_comb_sample = random.sample(all_xo_mut_combinations, 8)
 
-def run_ga_test(selection_func, crossover_func, mutation_func,
-                generations=100, pop_size=40, elitism=True, n_runs=30,
-                crossover_prob=0.9, mutation_prob=0.3):
-
-    all_runs_best_per_gen = []  #each element will be a list of fitnesses per generation
-
-    for run in range(n_runs):
-        population = [FootballSolution() for _ in range(pop_size)]
-        best_per_gen = []
-
-        for gen in range(generations):
-            new_population = []
-
-            if elitism:
-                elite = deepcopy(max(population, key=lambda ind: ind.fitness()))
-                new_population.append(elite)
-
-            while len(new_population) < pop_size:
-                p1 = selection_func(population)
-                p2 = selection_func(population)
-
-                if random.random() < crossover_prob:
-                    c1 = crossover_func(p1, p2)
-                else:
-                    c1= deepcopy(p1)
-
-                if random.random() < mutation_prob:
-                    c1 = mutation_func(None, c1)
-                # if random.random() < mutation_prob:
-                #     c2 = mutation_func(None, c2)
-
-                for child in [c1]:
-                    if len(new_population) < pop_size:
-                        new_population.append(child)
-
-            population = new_population
-
-            # keeps the best fitness of this gen
-            best_gen_fitness = max(ind.fitness() for ind in population)
-            best_per_gen.append(best_gen_fitness)
-
-        all_runs_best_per_gen.append(best_per_gen)
-
-    return all_runs_best_per_gen  #list of lists
+selection_combs = []
+for sel_name, sel_func in selection_methods.items():
+    for (c_name, c_func), (m_name, m_func) in xo_mut_comb_sample:
+        ga_runner = run_ga_test_two_children if "2child" in c_name else run_ga_test_single_child
+        for elitism_flag in [True, False]:
+            selection_combs.append((sel_name, sel_func, c_name, c_func, m_name, m_func, ga_runner, elitism_flag))
 
 
+
+# Parametrizações gerais
+generations = 10
+pop_size = 15
+n_runs = 10
+crossover_prob = 0.9
+mutation_prob = 0.1
 
 
 results_df = pd.DataFrame()
 
-for elitism_flag in [True, False]:
-    for sel_name, sel_func in selection_methods.items():
-        for cross_name, cross_func in crossover_methods.items():
-            for mut_name, mut_func in mutation_methods.items():
-                name = f"{sel_name}|{cross_name}|{mut_name}|elitism={elitism_flag}"
-                print(f"\nRunning {name} ...")
 
-                all_runs = run_ga_test(
-                    sel_func, cross_func, mut_func,
-                    generations=100,
-                    pop_size=40,
-                    n_runs=30,
-                    elitism=elitism_flag,
-                    crossover_prob=0.9,
-                    mutation_prob=0.1,
-    
-                )
 
-                #all_runs is a list of 30 lists, one per run
-                medians = np.median(np.transpose(all_runs), axis=1)
+for sel_name, sel_func, c_name, c_func, m_name, m_func, ga_runner, elitism_flag in selection_combs:
+    combination_name = f'{sel_name}|{c_name}|{m_name}|elitism={elitism_flag}'
+    print(f"\nRunning {combination_name} ...")
 
-                results_df[name] = medians
+    all_runs = ga_runner(
+        sel_func, c_func, m_func,
+        generations=generations,
+        pop_size=pop_size,
+        n_runs=n_runs,
+        elitism=elitism_flag,
+        crossover_prob=crossover_prob,
+        mutation_prob=mutation_prob
+    )
 
-# saves the csv file
-results_df.to_csv("ga_selection_analysis.csv", index_label="Generation")
+    medians = np.median(np.transpose(all_runs), axis=1)
+    results_df[combination_name] = medians
 
-print("\nResultados salvos em 'ga_selection_analysis.csv'")
-
+# Save results
+results_df.to_csv('ga_selection_analysis2.csv')
+print("\nResultados salvos em 'ga_selection_analysis2.csv'")
 
 
 
